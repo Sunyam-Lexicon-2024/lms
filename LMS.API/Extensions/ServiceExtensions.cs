ac0294559c606bc2b053dd8d40e12491fa4a460f
suny-am <visualarea.1@gmail.com>
2024-06-20 13:00:35 +0000

    do not create scoped DbContext

--- LMS.API/Extensions/ServiceExtensions.cs
+++ LMS.API/Extensions/ServiceExtensions.cs
@@ -1,7 +1,3 @@
-using FastEndpoints;
-using Lms.Data.DbContexts;
-using Microsoft.EntityFrameworkCore;
-
 namespace LMS.API.Extensions;
 
 public static class ServiceExtensions
@@ -10,8 +6,6 @@ public static class ServiceExtensions
     public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration config)
     {
         services.AddFastEndpoints();
-        services.AddDbContext<LmsDbContext>(options =>
-            options.UseSqlServer(config.GetConnectionString("Default")));
 
         services.AddDbContextFactory<LmsDbContext>(options =>
             options.UseSqlServer(config.GetConnectionString("Default")));
