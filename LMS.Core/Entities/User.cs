e63b68b757eb26039df5e197910d7914ab4ef9fb
suny-am <visualarea.1@gmail.com>
2024-06-23 16:37:11 +0000

    make parent User class concrete to make compatible with aspnet identity UserManager/UserStore

--- LMS.Core/Entities/User.cs
+++ LMS.Core/Entities/User.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace LMS.Core.Entities;
 
-public abstract class User : IdentityUser
+public class User : IdentityUser
 {
     public string Name { get; set; }
     public ICollection<Document> Documents { get; } = [];
