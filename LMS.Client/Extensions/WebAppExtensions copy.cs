de68b2389450007230e008392abc5004d3b0dc5f
suny-am <visualarea.1@gmail.com>
2024-06-24 22:00:24 +0000

    add extension classes for client project

---
+++ LMS.Client/Extensions/WebAppExtensions copy.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using LMS.Client.Components.Account;
+using LMS.Data.DbContexts;
+using LMS.Client.Stores;
+namespace LMS.Client.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        services.AddCascadingAuthenticationState();
+        services.AddScoped<IdentityUserAccessor>();
+        services.AddScoped<IdentityRedirectManager>();
+        services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddIdentityCookies();
+
+        services.AddDbContext<LmsDbContext>(options =>
+        {
+            if (Environment.GetEnvironmentVariable("CONTAINER_ENV") is not null)
+            {
+                options.UseSqlite(configuration.GetConnectionString("ContainerConnection"));
+            }
+            else
+            {
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            }
+        });
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddUserStore<LmsUserStore>()
+            .AddEntityFrameworkStores<LmsDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
+
+        services.AddSingleton<IEmailSender<User>, IdentityNoOpEmailSender>();
+
+        services.AddScoped(sp =>
+            new HttpClient()
+            {
+                BaseAddress = new Uri(configuration["API:BaseAddress"]!)
+            }
+        );
+
+        return services;
+    }
+}
