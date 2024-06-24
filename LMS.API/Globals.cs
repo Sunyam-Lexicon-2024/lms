8230d50667a379306c3730f524a1958ce2387059
suny-am <visualarea.1@gmail.com>
2024-06-24 20:00:15 +0000

    fix namespace typo

--- LMS.API/Globals.cs
+++ LMS.API/Globals.cs
@@ -1,6 +1,6 @@
 global using FastEndpoints;
 global using LMS.Core.Entities;
 global using LMS.Core.Models;
-global using Lms.Data.DbContexts;
+global using LMS.Data.DbContexts;
 global using Microsoft.EntityFrameworkCore;
 global using FastEndpoints.Swagger;
