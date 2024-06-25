1c45ce74b71dd6b0c362b23f0c739758c137becc
suny-am <visualarea.1@gmail.com>
2024-06-25 14:54:30 +0000

    remove legacy env reference

--- LMS.API/Extensions/WebAppExtensions.cs
+++ LMS.API/Extensions/WebAppExtensions.cs
@@ -8,7 +8,7 @@ public static class WebAppExtensions
     {
         app.UseDefaultExceptionHandler();
 
-        if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "DevContainers")
+        if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
 
