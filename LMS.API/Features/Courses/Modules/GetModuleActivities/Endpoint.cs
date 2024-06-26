bba2b619053563e84ad91fb9bf7c88244147ee09
suny-am <visualarea.1@gmail.com>
2024-06-26 10:30:24 +0000

    add endpoint description + summary

--- LMS.API/Features/Courses/Modules/GetModuleActivities/Endpoint.cs
+++ LMS.API/Features/Courses/Modules/GetModuleActivities/Endpoint.cs
@@ -8,6 +8,36 @@ public class Endpoint : Endpoint<Request, Results<Ok<Response>, NotFound, NoCont
     public override void Configure()
     {
         Get("/courses/modules/{ModuleId}/get-all-courses");
+          Description(d =>
+            d.Produces<Response>(200, "application/json")
+        );
+        // Swagger summary
+        Summary(s =>
+        {
+            s.Summary = "Gets all module activities.";
+            s.Description = "Gets all actitvities for a specific module (lookup on the module ID).";
+            s.ExampleRequest = new Request() { ModuleId = 1 };
+            s.ResponseExamples[200] = new Response()
+            {
+                CourseId = 1,
+                ModuleActivities = [
+                    new()
+                    {
+                        Name = "My module",
+                        Description = "First module description",
+                        StartDate = DateOnly.FromDateTime(DateTime.Today),
+                        EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(15))
+                    },
+                    new()
+                    {
+                        Name = "My second module",
+                        Description = "Second module description",
+                        StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(15)),
+                        EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(30))
+                    }
+                ]
+            };
+        });
     }
 
     public override async Task<Results<Ok<Response>, NotFound, NoContent>> ExecuteAsync(Request req, CancellationToken ct)
@@ -30,23 +60,18 @@ public class Endpoint : Endpoint<Request, Results<Ok<Response>, NotFound, NoCont
         {
             return TypedResults.NotFound();
         }
-        else
+
+        if (module.Activities.Count == 0)
         {
-            if (!module.Activities.Any())
-            {
-                return TypedResults.NoContent();
-            }
-            else
-            {
-
-            }
-            var activityModels = module.Activities.Select(Map.FromEntity);
-
-            return TypedResults.Ok<Response>(new()
-            {
-                CourseId = module.Parent.Id,
-                ModuleActivities = activityModels,
-            });
+            return TypedResults.NoContent();
         }
+
+        var activityModels = module.Activities.Select(Map.FromEntity);
+
+        return TypedResults.Ok<Response>(new()
+        {
+            CourseId = module.Parent.Id,
+            ModuleActivities = activityModels,
+        });
     }
 }
