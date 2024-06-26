48b41282aa180b68139b083564e2711b6588f45d
Y.Rezaee <122305716+rezapps@users.noreply.github.com>
2024-06-26 17:45:23 +0000

    Added feature/teacher-get-document-by-id, it uses the same DocumentBaseModel as feature/teacher-get-documents

---
+++ LMS.API/Features/Users/Teachers/GetDocumentById/Mapper.cs
@@ -0,0 +1,17 @@
+namespace Users.Teachers.GetDocumentById;
+
+public class Mapper : ResponseMapper<DocumentBaseModel, Document>
+{
+    public override DocumentBaseModel FromEntity(Document d)
+    {
+        return new DocumentBaseModel()
+        {
+            Id = d.Id,
+            Name = d.Name,
+            Url = d.Url,
+            Description = d.Description,
+            UploadedAt = d.UploadedAt,
+            UserId = d.UploaderId,
+        };
+    }
+}
