9c8dde4f6c51342bb77e6b05bdc7b21450584748
House <House@DESKTOP-ON4A8O9>
2024-06-27 02:07:36 +0200

    added endpoint student/course/modules/{moduleId}

---
+++ LMS.API/Features/Users/Students/GetModuleDetails/Endpoint.cs
@@ -0,0 +1,84 @@
+﻿using FastEndpoints;
+using LMS.Data.DbContexts;
+using LMS.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Users.Students.GetAllStudents;
+
+namespace Users.Students.GetModuleDetails;
+
+
+//public class Endpoint : EndpointWithoutRequest<Response, Mapper>
+//{
+//    public override void Configure()
+//    {
+//        Get("/api/student/course/modules/{moduleId}");
+//    }
+
+//    public override async Task HandleAsync(CancellationToken ct)
+//    {
+//        IDbContextFactory<LmsDbContext>? contextFactory = TryResolve<IDbContextFactory<LmsDbContext>>();
+
+//        if (contextFactory is null)
+//        {
+//            ThrowError("Could not resolve DbContextFactory Service");
+//        }
+
+//        var moduleId = Route<int>("moduleId");
+
+//        using var context = contextFactory.CreateDbContext();
+//        var module = await context.CourseElements
+//                                  .FirstOrDefaultAsync(m => m.Id == moduleId, ct);
+
+//        if (module is null)
+//        {
+//            ThrowError("Module not found");
+//        }
+
+//        //var moduleModel = Map.FromEntity(Module);
+//        //var moduleModel = module.Select(Map.FromEntity).ToList();
+
+//        var moduleModel = Map.FromEntity(module);
+
+
+//        await SendAsync(new Response
+//        {
+//            Module = moduleModel
+//        }, cancellation: ct);
+//    }
+//}
+
+public class Endpoint : EndpointWithoutRequest<Response, Mapper>
+{
+    public override void Configure()
+    {
+        Get("/student/course/modules/{moduleId}");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        IDbContextFactory<LmsDbContext>? contextFactory = TryResolve<IDbContextFactory<LmsDbContext>>();
+
+        if (contextFactory is null)
+        {
+            ThrowError("Could not resolve DbContextFactory Service");
+        }
+
+        var moduleId = Route<int>("moduleId");
+
+        using var context = contextFactory.CreateDbContext();
+        var module = await context.CourseElements
+                                  .FirstOrDefaultAsync(m => m.Id == moduleId, ct);
+
+        if (module is null)
+        {
+            ThrowError("Module not found");
+        }
+
+        var moduleModel = Map.FromEntity(module);
+
+        await SendAsync(new Response
+        {
+            Module = moduleModel
+        }, cancellation: ct);
+    }
+}
