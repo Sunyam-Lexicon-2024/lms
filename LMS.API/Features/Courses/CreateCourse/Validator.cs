f10a93e03ed7e4c3b37e48902dec985e51bee641
Linda <linda@hellomynameislinda.com>
2024-06-27 14:59:22 +0200

    Updated formatting for easier reading.

--- LMS.API/Features/Courses/CreateCourse/Validator.cs
+++ LMS.API/Features/Courses/CreateCourse/Validator.cs
@@ -12,6 +12,7 @@ public class Validator : Validator<CoursePostModel>
 
         RuleFor(c => c.StartDate)
             .NotEmpty().WithMessage("Start date is required to follow the format \"YYYY-MM-DD\"!");
+
         RuleFor(c => c.EndDate)
             .NotEmpty().WithMessage("End date is required to follow the format \"YYYY-MM-DD\"!")
             .GreaterThanOrEqualTo(c => c.StartDate).WithMessage("End date must not occur before the start date!");
