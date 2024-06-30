4c9169d74a405c476bfce24cac580e55cac41bf5
Tina Bruce <tina@fishface.se>
2024-06-30 14:33:47 +0200

    Add client component for creating a new module

---
+++ LMS.Core/Models/ModuleCreateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.Core.Models
+{
+    public class ModuleCreateModel
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int ParentId { get; set; }
+        public required DateOnly StartDate { get; set; }
+        public required DateOnly EndDate { get; set; }
+    }
+}
+ 
