d6f352d9839fa66e68dbc6e39bc2d5bc61758b7f
Tina Bruce <tina@fishface.se>
2024-06-30 18:48:25 +0200

    Add client component for adding a new module

--- LMS.Core/Models/CourseBaseModel.cs
+++ LMS.Core/Models/CourseBaseModel.cs
@@ -2,7 +2,11 @@ namespace LMS.Core.Models;
 
 public class CourseBaseModel
 {
-    public int? CourseId { get; set; } = default!;
+    //public int? CourseId { get; set; } = default!;
+    public int CourseId { get; set; } = default!;
     public string? Name { get; set; } = default!;
     public string? Description { get; set; } = default!;
+
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
 }
--- LMS.Core/Models/ModuleCreateModel.cs
+++ LMS.Core/Models/ModuleCreateModel.cs
@@ -2,11 +2,11 @@
 {
     public class ModuleCreateModel
     {
-        public required string Name { get; set; }
+        public string Name { get; set; }
         public string? Description { get; set; }
         public int ParentId { get; set; }
-        public required DateOnly StartDate { get; set; }
-        public required DateOnly EndDate { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
     }
 }
  
