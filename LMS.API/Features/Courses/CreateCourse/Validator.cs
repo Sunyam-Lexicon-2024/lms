67251fb7c32e4cf1664b2b7786965a1d8ceba0cb
Linda <linda@hellomynameislinda.com>
2024-07-01 11:35:31 +0200

    Minor validation updates according på code-review.

--- LMS.API/Features/Courses/CreateCourse/Validator.cs
+++ LMS.API/Features/Courses/CreateCourse/Validator.cs
@@ -6,7 +6,8 @@ public class Validator : Validator<CoursePostModel>
     {
         RuleFor(c => c.Name)
             .NotEmpty().WithMessage("A course name is required!")
-            .MaximumLength(255).WithMessage("The course name is too long");
+            .MaximumLength(255).WithMessage("The course name is too long!")
+            .MinimumLength(255).WithMessage("The course name must be at least 3 chractrs to be identifiable!");
 
         RuleFor(c => c.Description)
             .NotEmpty().WithMessage("A description is required!");
@@ -14,7 +15,7 @@ public class Validator : Validator<CoursePostModel>
         RuleFor(c => c.StartDate)
             .NotEmpty().WithMessage("Start date is required to follow the format \"YYYY-MM-DD\"!")
             .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow))
-            .WithMessage("Activity end date must be current or future date");
+            .WithMessage("Activity end date must be current or future date!");
 
         RuleFor(c => c.EndDate)
             .NotEmpty().WithMessage("End date is required to follow the format \"YYYY-MM-DD\"!")
--- LMS.Core/Models/CourseCreateModel.cs
+++ LMS.Core/Models/CourseCreateModel.cs
@@ -5,13 +5,8 @@ namespace LMS.Core.Models;
 
 public class CourseCreateModel
 {
-    [Required]
-    [MinLength(3, ErrorMessage = "The name must be at least 3 charactrs to be identifiable")]
     public string Name { get; set; }
-    [Required]
     public string? Description { get; set; }
-    [Required]
     public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
-    [Required]
     public DateOnly EndDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 }
